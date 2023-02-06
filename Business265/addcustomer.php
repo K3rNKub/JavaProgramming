5a12cf8e6868f1e00b375caee82b1f9dcc92db3d
K3rNKub <111554758+K3rNKub@users.noreply.github.com>
2023-02-06 12:16:31 +0700

    Add files via upload

---
+++ Business265/addcustomer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add customer</title>
+</head>
+<body>
+    <h1>Add Customer</h1>
+    <form action="addcustomer.php" method="POST">
+        <input type="text" placeholder="Enter Customer ID" name="CustomerID"/>
+        <br><br>
+        <input type="text" placeholder="Name" name="Name"/>
+        <br><br>
+        <input type="date" placeholder="Name" name="Birthdate"/>
+        <br><br>
+        <input type="text" placeholder="Email" name="Email"/>
+        <br><br>
+        <input type="text" placeholder="Country Code" name="CountryCode"/>
+        <br><br>
+        <input type="text" placeholder="OutStanding debt" name="OutStandingDebt"/>
+        <input type="submit">
+    </form>
+</body>
+</html>
+
+<?php
+if(!empty($_POST['CustomerID']) && !empty($_POST['Name']) && !empty($_POST['Birthdate']) && !empty($_POST['Email']) && !empty($_POST['CountryCode']) && !empty($_POST['OutStandingDebt'])):
+    require 'connect.php';
+    $sql_insert ="insert into customer values(:CustomerID, :Name, :Birthdate, :Email, :CountryCode, :OutStandingDebt)";
+
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindParam(':CustomerID',$_POST['CustomerID']);
+    $stmt->bindParam(':Name',$_POST['Name']);
+    $stmt->bindParam(':Birthdate',$_POST['Birthdate']);
+    $stmt->bindParam(':Email',$_POST['Email']);
+    $stmt->bindParam(':CountryCode',$_POST['CountryCode']);
+    $stmt->bindParam(':OutStandingDebt',$_POST['OutStandingDebt']);
+    if($stmt->execute()):
+        $message ='Suscessfully add new customer';
+    else:
+        $message ='Fail to add new customer';
+    endif;
+    echo $message;
+    $conn = null;
+    endif;
+?>
